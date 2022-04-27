479fe6472bc5c30e7047b992895e4a1b61eaf5c6
Tatiana <ganzhinovat@yandex.ru>
2022-04-27 23:06:41 +0300

    feat: add the solution of 'carbon-dating' task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,35 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  //Активность (т.е. скорость распада = ΔN / Δt (формула 1)
+  //Активность = k* N (формула 2)
+  //=> ΔN/Δt = k * N (соединяем формула 1 = формула 2)
+  //=> ln(N/N0) = k * t (переписано по интегральному закону через логарифм)
+  //=> t = ln(N/N0)/k (выводим нужное нам t - примерное время распада) (Формула 3)
+
+  //HALF_LIFE_PERIOD = 0,693 / k (формула 4)
+  //из нее k = 0.693 / HALF_LIFE_PERIOD
+
+  //Подставляем в формулу 3 наш вывод из формулы 4:
+  //t = ln(N/N0)/k = ln(N/N0)/(0.693 / HALF_LIFE_PERIOD)
+
+  let log = 0.693;
+
+  if (typeof sampleActivity !== 'string' ||
+    +sampleActivity < 0 ||
+    +sampleActivity === 0 ||
+    isNaN(+sampleActivity)) {
+
+    return false;
+
+  } else {
+    let k = log / HALF_LIFE_PERIOD;
+    let activity = Math.log(MODERN_ACTIVITY / +sampleActivity);
+    let t = Math.ceil(activity / k);
+
+    return (t < 0) ? false : t;
+  }
 }
 
 module.exports = {
