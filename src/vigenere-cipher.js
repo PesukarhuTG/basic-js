e8ea432b8bf5c5293cd22ad073baba96527e70e5
Tatiana <ganzhinovat@yandex.ru>
2022-04-29 13:43:30 +0300

    feat: implement the solution of 'vigenere-cipher' task - part 2 with decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -58,13 +58,44 @@ class VigenereCipheringMachine {
       encode[i] = String.fromCharCode(encode[i]);
     }
 
-    //console.log(encode.join('').toUpperCase());
-    return encode.join('').toUpperCase();
-
+    return (this.direct === false) ? encode.reverse().join('').toUpperCase() : encode.join('').toUpperCase();
   }
 
   decrypt(message, key) {
     if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let inputMessage = Array.from(String(message).toLowerCase());
+    let arrayFromKey = Array.from(String(key).toLowerCase());
+    let charCodeInputMessage = [];
+    let encode = [];
+
+    //переводим символы абракадабры в charCode
+    for (let i = 0; i < inputMessage.length; i++) {
+      charCodeInputMessage.push(inputMessage[i].charCodeAt());
+    }
+
+    //пробегаемся по массиву исходных charCodes, проверяя только буквы
+    for (let i = 0, j = 0; i < charCodeInputMessage.length; i++) {
+      if (charCodeInputMessage[i] > 96 && charCodeInputMessage[i] < 123) {
+        let newChar = (charCodeInputMessage[i] - arrayFromKey[j].charCodeAt() + 97) < 97 ?
+          (charCodeInputMessage[i] - arrayFromKey[j].charCodeAt() + 97 + 26) :
+          (charCodeInputMessage[i] - arrayFromKey[j].charCodeAt() + 97);
+
+        encode.push(newChar);
+        j++;
+        j = (j > arrayFromKey.length - 1) ? (j - arrayFromKey.length) : j;
+      } else {
+        //где не буквы, просто переносим код
+        encode.push(charCodeInputMessage[i]);
+      }
+    }
+
+    //из новых charCodes геренируем "реальные" буквы
+    for (let i = 0; i < encode.length; i++) {
+      encode[i] = String.fromCharCode(encode[i]);
+    }
+
+    return (this.direct === false) ? encode.reverse().join('').toUpperCase() : encode.join('').toUpperCase();
   }
 }
 
