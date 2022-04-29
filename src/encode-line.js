bfc4a04b64a9ea6ce5a12a401a3923415b3b41b4
Tatiana <ganzhinovat@yandex.ru>
2022-04-29 22:00:03 +0300

    feat: add the solution of 'encode-line' task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = str.split('');
+
+  for (let i = 0; i < result.length; i++) {
+    result[i] = Array.from(result[i]); //создаем подмассив из каждой буквы
+    result[i].unshift(1); //добавляем счетчик
+  }
+
+  for (let i = 0; i < result.length - 1;) {
+    if (result[i][1] === result[i + 1][1]) {
+      result[i][0]++; //увеличиваем счетчик
+      result.splice(result.indexOf(result[i + 1]), 1); //удаляем дубль-элемент
+      i = i;
+    } else {
+      i++;
+    }
+  }
+
+  //избавляемя от '1' перед элементами
+  for (let i = 0; i < result.length; i++) {
+    if (result[i][0] === 1) {
+      result[i] = [result[i][1]];
+    }
+    result[i] = result[i].join('');
+  }
+
+  return result.join('');
 }
 
 module.exports = {
