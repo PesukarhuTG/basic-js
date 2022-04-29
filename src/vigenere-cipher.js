a52c4582e5f3091bd63bd93d3bdcbdfdfa289589
Tatiana <ganzhinovat@yandex.ru>
2022-04-29 12:09:41 +0300

    feat: implement the solution of 'vigenere-cipher' task - part 1 with encypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let inputMessage = Array.from(String(message).toLowerCase());
+    let arrayFromKey = Array.from(String(key).toLowerCase());
+    let charCodeInputMessage = [];
+    let encode = [];
+
+    //переводим символы в charCode
+    for (let i = 0; i < inputMessage.length; i++) {
+      charCodeInputMessage.push(inputMessage[i].charCodeAt());
+    }
+
+    //пробегаемся по массиву исходных charCodes, проверяя только буквы
+    for (let i = 0, j = 0; i < charCodeInputMessage.length; i++) {
+      if (charCodeInputMessage[i] > 96 && charCodeInputMessage[i] < 123) {
+        let newChar = (charCodeInputMessage[i] + arrayFromKey[j].charCodeAt() - 97) > 122 ?
+          (charCodeInputMessage[i] + arrayFromKey[j].charCodeAt() - 97 - 26) :
+          (charCodeInputMessage[i] + arrayFromKey[j].charCodeAt() - 97);
+
+        encode.push(newChar);
+        j++;
+        j = (j > arrayFromKey.length - 1) ? (j - arrayFromKey.length) : j;
+      } else {
+        //где не буквы, просто переносим код
+        encode.push(charCodeInputMessage[i]);
+      }
+    }
+
+    //из новых charCodes геренируем буквы
+    for (let i = 0; i < encode.length; i++) {
+      encode[i] = String.fromCharCode(encode[i]);
+    }
+
+    //console.log(encode.join('').toUpperCase());
+    return encode.join('').toUpperCase();
+
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
   }
 }
 
