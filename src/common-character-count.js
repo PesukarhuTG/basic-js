8ec678f0d0dedb019d94a5c4f1cabfdac15d9aae
Tatiana <ganzhinovat@yandex.ru>
2022-04-29 19:53:30 +0300

    feat: add the solution of 'common-charter' task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  if (s1.length === 0 || s2.length === 0) 0;
+
+  let counter = 0;
+  let mainString = (s1.length > s2.length) ? s1.split('') : s2.split('');
+  let comparedStr = (s1.length <= s2.length) ? s1 : s2;
+
+  //пробегаемся по сравниваемой строке и проверяем вхождение в главную строку
+  for (let i = 0; i < comparedStr.length; i++) {
+    let index = mainString.indexOf(comparedStr[i]);
+
+    if (index > -1) {
+      counter++;
+      mainString[index] = '';
+    }
+  }
+
+  return counter;
 }
 
 module.exports = {
