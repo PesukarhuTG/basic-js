94679198d301d21a3c910d5e33cb315509395324
Tatiana <ganzhinovat@yandex.ru>
2022-04-30 13:50:05 +0300

    feat: add the solution of 'mine-sweeper' task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  //глубокая копия массива
+  const minesArea = JSON.parse(JSON.stringify(matrix));
+
+  //каждый элемент обнуляем для дальнейшего расчета
+  for (let subArray of minesArea) {
+    for (let i = 0; i < subArray.length; i++) {
+      subArray[i] = 0;
+    }
+  }
+
+  //проходимся по каждой строчке исходного массива
+  for (let i = 0; i < matrix.length; i++) {
+    //проходимся по каждому элементу подмассива
+    for (let j = 0; j < matrix[i].length; j++) {
+      //2)проверяем относительно предыдущей строки
+      if (matrix[i - 1] && matrix[i - 1][j] === true) minesArea[i][j] += 1;
+      if (matrix[i - 1] && matrix[i - 1][j + 1] === true) minesArea[i][j] += 1;
+      if (matrix[i - 1] && matrix[i - 1][j - 1] === true) minesArea[i][j] += 1;
+
+      //1) проверяем относительно центральной строки
+      //if (matrix[i] && matrix[i][j] === true) result[i][j] +=1;
+      if (matrix[i] && matrix[i][j + 1] === true) minesArea[i][j] += 1;
+      if (matrix[i] && matrix[i][j - 1] === true) minesArea[i][j] += 1;
+
+      //3)проверяем относительно следующей строки
+      if (matrix[i + 1] && matrix[i + 1][j] === true) minesArea[i][j] += 1;
+      if (matrix[i + 1] && matrix[i + 1][j + 1] === true) minesArea[i][j] += 1;
+      if (matrix[i + 1] && matrix[i + 1][j - 1] === true) minesArea[i][j] += 1;
+    }
+  }
+  return minesArea;
 }
 
 module.exports = {
